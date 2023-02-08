8fbc420584a8ecdf36ef19fe57cca48c7b7bf545
BraulioJose18 <40570187+BraulioJose18@users.noreply.github.com>
2023-02-08 17:25:23 -0500

    Expiration and Packs

--- apps/user/api/urls.py
+++ apps/user/api/urls.py
@@ -1,3 +1,4 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from apps.user.api.views import *
@@ -7,4 +8,9 @@ router.register(r'api/user', UserViewSet, basename='User')
 router.register(r'api/role', RoleViewSet, basename='Role')
 router.register(r'api/permission', PermissionViewSet, basename='Permission')
 urlpatterns = router.urls
+# urlpatterns += [
+#     path("api-auth/", include("rest_framework.urls")),
+# ]
+# Swagger settings
+
 
